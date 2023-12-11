fdc6b8f56f062a6c6a3ee987537a813f561b0be3
r0930514 <r0930514@gmail.com>
2023-12-12 02:56:43 +0800

    訂單明細Card設計

--- app/src/main/java/com/r0930514/fastfoodorderapp/screens/shoppingCartScreen/ShoppingCartScreen.kt
+++ app/src/main/java/com/r0930514/fastfoodorderapp/screens/shoppingCartScreen/ShoppingCartScreen.kt
@@ -4,49 +4,20 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.ArrowBack
-import androidx.compose.material3.CenterAlignedTopAppBar
 import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.Icon
-import androidx.compose.material3.IconButton
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.vector.ImageVector
-import androidx.compose.ui.res.vectorResource
 import androidx.navigation.NavHostController
-import com.r0930514.fastfoodorderapp.R
 import com.r0930514.fastfoodorderapp.screens.shoppingCartScreen.components.CartCard
-import com.r0930514.fastfoodorderapp.ui.theme.TopDefaultAppBarColor
+import com.r0930514.fastfoodorderapp.screens.shoppingCartScreen.components.ShoppingCartAppBar
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun ShoppingCart(navHostController: NavHostController){
     Scaffold (
         topBar = {
-            CenterAlignedTopAppBar(
-                title = { Text(text = "購物車") },
-                colors = TopDefaultAppBarColor(),
-                navigationIcon = {
-                    IconButton(
-                        onClick = {
-                            navHostController.popBackStack()
-                        }
-                    ) {
-                        Icon(imageVector = Icons.Filled.ArrowBack, contentDescription = "關閉", )
-                    }
-                },
-                actions = {
-                    IconButton(onClick = { /*TODO*/ }) {
-                        Icon(
-                            imageVector = ImageVector.Companion.vectorResource(id = R.drawable.remove_shopping_cart),
-                            contentDescription = "清空購物車"
-                        )
-                    }
-                }
-            )
+            ShoppingCartAppBar(navHostController)
         }
     ){
         Column (modifier = Modifier.padding(it)){
---
+++ app/src/main/java/com/r0930514/fastfoodorderapp/screens/shoppingCartScreen/components/ShoppingCartAppBar.kt
@@ -0,0 +1,41 @@
+package com.r0930514.fastfoodorderapp.screens.shoppingCartScreen.components
+
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material3.CenterAlignedTopAppBar
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.res.vectorResource
+import androidx.navigation.NavHostController
+import com.r0930514.fastfoodorderapp.R
+import com.r0930514.fastfoodorderapp.ui.theme.TopDefaultAppBarColor
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun ShoppingCartAppBar(navHostController: NavHostController){
+    CenterAlignedTopAppBar(
+        title = { Text(text = "購物車") },
+        colors = TopDefaultAppBarColor(),
+        navigationIcon = {
+            IconButton(
+                onClick = {
+                    navHostController.popBackStack()
+                }
+            ) {
+                Icon(imageVector = Icons.Filled.ArrowBack, contentDescription = "關閉", )
+            }
+        },
+        actions = {
+            IconButton(onClick = { /*TODO*/ }) {
+                Icon(
+                    imageVector = ImageVector.vectorResource(id = R.drawable.remove_shopping_cart),
+                    contentDescription = "清空購物車"
+                )
+            }
+        }
+    )
+}
