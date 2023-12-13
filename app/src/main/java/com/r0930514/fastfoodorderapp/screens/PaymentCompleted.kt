5825c3bafb902170a826d5d779bf0a6a246709d2
r0930514 <r0930514@gmail.com>
2023-12-13 22:33:26 +0800

    付款頁面修復

--- app/src/main/java/com/r0930514/fastfoodorderapp/screens/PaymentCompleted.kt
+++ app/src/main/java/com/r0930514/fastfoodorderapp/screens/PaymentCompleted.kt
@@ -1,22 +1,16 @@
 package com.r0930514.fastfoodorderapp.screens
 
-import android.annotation.SuppressLint
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.consumedWindowInsets
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.width
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowBack
-import androidx.compose.material.icons.filled.CheckCircle
-import androidx.compose.material3.Button
+import androidx.compose.material.icons.filled.Check
 import androidx.compose.material3.CenterAlignedTopAppBar
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.ExtendedFloatingActionButton
 import androidx.compose.material3.FabPosition
-import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.Scaffold
@@ -26,15 +20,12 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.layout.ContentScale
-import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.vectorResource
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavHostController
 import com.r0930514.fastfoodorderapp.R
-import com.r0930514.fastfoodorderapp.screens.mainScreen.component.CAppBar
 import com.r0930514.fastfoodorderapp.ui.theme.TopDefaultAppBarColor
 
-@SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun PaymentCompleted(navHostController: NavHostController) {
@@ -52,47 +43,35 @@ fun PaymentCompleted(navHostController: NavHostController) {
                 }
             )
         },
-
         floatingActionButtonPosition = FabPosition.End,
         floatingActionButton = {
-            FloatingActionButton(onClick = { /*TODO*/ }) {
-                Row(verticalAlignment = Alignment.CenterVertically) {
-                    Icon(imageVector = Icons.Filled.CheckCircle, contentDescription = "Check")
-                    Spacer(modifier = Modifier.width(8.dp))
-                    Text(text = "結束")
-                }
-            }
+            ExtendedFloatingActionButton(
+                icon = {
+                    Icon(imageVector = Icons.Filled.Check, contentDescription = "Check")
+                },
+                text = {
+                    Text(text = "完成")
+                },
+                onClick = {}
+            )
         }
-
     ) {
-        Column {
-            Column {
-                Column(
-                    modifier = Modifier
-                        .padding(12.dp)
-                        .fillMaxSize(),
-                    horizontalAlignment = Alignment.CenterHorizontally
+        Column (
+            modifier = Modifier.padding(it)
+        ){
+            Column(
+                modifier = Modifier
+                    .padding(12.dp)
+                    .fillMaxSize(),
+                horizontalAlignment = Alignment.CenterHorizontally
 
+            )
+            {
+                Image(
+                    imageVector = ImageVector.Companion.vectorResource(R.drawable.fastfood),
+                    contentDescription = null,
+                    contentScale = ContentScale.FillBounds
                 )
-                {
-                    Image(
-                        painterResource(id = R.drawable.fastfood),
-                        contentDescription = null,
-                        contentScale = ContentScale.FillBounds
-                    )
-                    
-//                    Icon(
-//                        imageVector = ImageVector.Companion.vectorResource(id = R.drawable.fastfood),
-//                        contentDescription = null
-//                    )
-//                    Text(text = "請登入會員")
-//                    Button(onClick = { navHostController.navigate("Login") }) {
-//                        Text(text = "登入")
-//                    }
-//                    Button(onClick = { navHostController.navigate("PaymentCompleted") }) {
-//                        Text(text = "測試")
-//                    }
-                }
             }
         }
     }
