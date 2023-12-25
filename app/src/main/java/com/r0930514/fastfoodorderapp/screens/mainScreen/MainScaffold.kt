f6a27c9aafba5146e670aef8b00d9ff7f47196fc
r0930514 <r0930514@gmail.com>
2023-12-26 03:03:49 +0800

    驗證登入狀態後才可以打開購物車

--- app/src/main/java/com/r0930514/fastfoodorderapp/screens/mainScreen/MainScaffold.kt
+++ app/src/main/java/com/r0930514/fastfoodorderapp/screens/mainScreen/MainScaffold.kt
@@ -1,5 +1,6 @@
 package com.r0930514.fastfoodorderapp.screens.mainScreen
 
+import android.widget.Toast
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.rememberLazyListState
@@ -8,6 +9,7 @@ import androidx.compose.material3.Icon
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableIntStateOf
 import androidx.compose.runtime.saveable.rememberSaveable
@@ -17,10 +19,14 @@ import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.res.vectorResource
 import androidx.navigation.NavHostController
 import com.r0930514.fastfoodorderapp.R
+import com.r0930514.fastfoodorderapp.dataStore
 import com.r0930514.fastfoodorderapp.screens.mainScreen.component.CNavBar
+import com.r0930514.fastfoodorderapp.viewModels.UserStateViewModel
 
 @Composable
 fun MainScaffold(navController: NavHostController) {
+    val userStateViewModel = UserStateViewModel(navController.context.dataStore)
+    val username by userStateViewModel.userName.collectAsState()
     var selectedItem by rememberSaveable { mutableIntStateOf(0) }
     val scrollState = rememberLazyListState()
     Scaffold(
@@ -28,7 +34,12 @@ fun MainScaffold(navController: NavHostController) {
         floatingActionButton = {
             if (selectedItem == 1){
                 ExtendedFloatingActionButton(
-                    onClick = { navController.navigate("ShoppingCart") },
+                    onClick = {
+                              if(username=="") {
+                                  Toast.makeText(navController.context, "請先登入", Toast.LENGTH_SHORT).show()
+                                  navController.navigate("Login")
+                              } else navController.navigate("ShoppingCart")
+                    },
                     icon = {
                         Icon(
                             imageVector = ImageVector.vectorResource(id = R.drawable.shopping_cart),
