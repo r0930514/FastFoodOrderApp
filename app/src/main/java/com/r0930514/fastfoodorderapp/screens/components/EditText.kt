f820903450b399cfd5f4406ac78176fd1b51645f
r0930514 <r0930514@gmail.com>
2024-01-02 01:46:32 +0800

    改良鍵盤失焦

--- app/src/main/java/com/r0930514/fastfoodorderapp/screens/components/EditText.kt
+++ app/src/main/java/com/r0930514/fastfoodorderapp/screens/components/EditText.kt
@@ -2,6 +2,7 @@ package com.r0930514.fastfoodorderapp.screens.components
 
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.text.KeyboardActions
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Phone
@@ -9,13 +10,16 @@ import androidx.compose.material3.Icon
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.platform.LocalSoftwareKeyboardController
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.text.input.PasswordVisualTransformation
 import androidx.compose.ui.text.input.VisualTransformation
 import androidx.compose.ui.unit.dp
 
+@OptIn(ExperimentalComposeUiApi::class)
 @Composable
 fun EditText(
     value: String,
@@ -29,7 +33,7 @@ fun EditText(
     supportingText: String,
     leadingIcon: ImageVector = Icons.Filled.Phone
 ) {
-
+    val keyboardController = LocalSoftwareKeyboardController.current
     OutlinedTextField(
         value = value,
         onValueChange =  { onValueChange(it) },
@@ -43,6 +47,11 @@ fun EditText(
             contentDescription = null
         )},
         isError = isError,
+        keyboardActions = KeyboardActions(
+            onDone = {
+                keyboardController?.hide()
+            }
+        ),
         visualTransformation = if (keyboardType == KeyboardType.Password) PasswordVisualTransformation() else VisualTransformation.None
     )
 }
